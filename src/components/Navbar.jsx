17498ca585f32dc178cffebabf069506da030ca9
rockmel03 <kamalswag2@gmail.com>
2024-04-26 22:07:51 +0530

    created navbar & maked it responsive also

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbaar = () => {
-  return (
-    <div>Navbaar</div>
-  )
-}
+  const [toggle, setToggle] = useState(false);
 
-export default Navbaar
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px] " />
+      <ul className="hidden list-none sm:flex justify-end items-center flex-1">
+        {navLinks?.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins text-white ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${nav.link}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul
+            className="list-none flex flex-col justify-start items-center
+            flex-1"
+          >
+            {navLinks?.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins text-white ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                }`}
+              >
+                <a href={`#${nav.link}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbaar;
