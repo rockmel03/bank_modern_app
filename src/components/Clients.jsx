9db4cc2bd4a0ca9a4e199061a55ea2cc48708db2
rockmel03 <kamalswag2@gmail.com>
2024-04-28 01:19:35 +0530

    added client & testimonial section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { clients } from "../constants";
+import styles from "../style";
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
+const Clients = () => (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:w-[192px] min-w-[120px]`}>
+            <img src={client.logo} alt="client" className="w-[100px] sm:w-[192px] object-contain" />
+          </div>
+        ))}
+      </div>
+    </section>
   )
-}
+
 
 export default Clients
